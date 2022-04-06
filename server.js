7ad4f8e6bbc837bcaf51e89f8eb91e210c2af3d6
Pasi Härme <pasi.harme@gmail.com>
2022-04-06 19:39:44 +0300

    Write content-type header

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ const baseFolder = path.resolve(__dirname, 'www')
 const indexFile = baseFolder + '/index.html'
 const port = 8080
 
+const mimeTypes = {
+  '.html': 'text/html',
+  '.ico': 'image/x-icon',
+  '.jpeg': 'image/jpeg',
+  '.jpg': 'image/jpeg',
+  '.js': 'application/javascript',
+  '.png': 'image/x-png',
+  '.txt': 'text/plain',
+}
+
+const getMimeType = (url) => {
+  const ext = url.substring(url.lastIndexOf('.'))
+  return mimeTypes[ext]
+}
+
+const writeResponse = (res, data, mimeType) => {
+  res.writeHead(200, mimeType ? { 'Content-Type': mimeType } : undefined)
+  res.end(data)
+}
+
 fs.readFile(indexFile, (err, index) => {
   if (err) {
     console.error('Cound not load index.html from ' + indexFile)
@@ -15,14 +35,13 @@ fs.readFile(indexFile, (err, index) => {
     const file = baseFolder + req.url
     fs.readFile(file, (err, data) => {
       if (err) {
-        console.log('Could not find ' + file + ', falling back to ' + indexFile)
-        res.writeHead(200)
-        res.end(index)
+        console.log(`Could not find ${file}, falling back to ${indexFile}`)
+        writeResponse(res, index, mimeTypes['.html'])
         return
       }
-      console.log('Serving ' + file)
-      res.writeHead(200)
-      res.end(data)
+      const mimeType = getMimeType(req.url)
+      console.log(`Serving ${file} (${mimeType})`)
+      writeResponse(res, data, mimeType)
     })
   }).listen(port)
   console.log('Server listening at port ' + port)
