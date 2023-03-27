ba04cdad3adae7a790a24ccaa9befd18867ec8bb
Pasi Härme <pasi.harme@gmail.com>
2023-03-28 00:47:16 +0300

    updates to Gridv2

--- src/components/Soundboard.tsx
+++ src/components/Soundboard.tsx
@@ -7,7 +7,7 @@ import {
     CardActionArea,
     FormControlLabel,
     FormLabel,
-    Grid,
+    Unstable_Grid2 as Grid,
     Menu,
     MenuItem,
     Paper,
@@ -91,7 +91,7 @@ const Soundboard: React.FC = () => {
     return (
         <Paper sx={{ p: 4 }}>
             <Grid container spacing={4}>
-                <Grid item xs={12} sm={6}>
+                <Grid xs={12} sm={6}>
                     <FormLabel component='legend'>Group sounds</FormLabel>
                     <RadioGroup row value={groupBy} onChange={onGroupByChange}>
                         <FormControlLabel
@@ -108,7 +108,7 @@ const Soundboard: React.FC = () => {
                         />
                     </RadioGroup>
                 </Grid>
-                <Grid item xs={12} sm={6}>
+                <Grid xs={12} sm={6}>
                     <Autocomplete<string, true>
                         multiple
                         size='small'
@@ -124,7 +124,7 @@ const Soundboard: React.FC = () => {
                         )}
                     />
                 </Grid>
-                <Grid item xs={12} sm={6}>
+                <Grid xs={12} sm={6}>
                     <FormLabel component='legend'>Volume</FormLabel>
                     <Slider
                         defaultValue={defVolume}
@@ -134,8 +134,8 @@ const Soundboard: React.FC = () => {
                         onChangeCommitted={(_event, vol) => setVolume(vol as number)}
                     />
                 </Grid>
-                <Grid container item xs={12} sm={6} spacing={2} alignItems='flex-end'>
-                    <Grid item xs={10}>
+                <Grid container xs={12} sm={6} spacing={2} alignItems='flex-end'>
+                    <Grid xs={10}>
                         <TextField
                             size='small'
                             value={url}
@@ -145,7 +145,7 @@ const Soundboard: React.FC = () => {
                         />
 
                     </Grid>
-                    <Grid item xs={2}>
+                    <Grid xs={2}>
                         <Button
                             fullWidth
                             size='medium'
@@ -253,7 +253,7 @@ const Board: React.FC<BoardProps> = (props) => {
                     <Divider>{key}</Divider>
                     <Grid container>
                         {groupedSounds[key].map(({ name }) =>
-                            <Grid key={name} item xs={4} sm={3} md={2} >
+                            <Grid key={name} xs={4} sm={3} md={2} >
                                 <Box mr={1} mt={1}>
                                     <CardActionArea
                                         onContextMenu={handleContextClick(name)}
